d200b83ffc3f576ae324a3222431248ea95b6f50
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-12 10:23:39 +0300

    Done.

--- index.js
+++ index.js
@@ -62,6 +62,9 @@ const record = [
   ]
 
   superbowlWin = (record) => {
+
     const result = record.find( record => record.result === "W" );
+
     return !!result ? result.year : undefined;
+    
   }
